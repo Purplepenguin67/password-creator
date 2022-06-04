38c513bfadfc73278b6a92d256687eb0229dbb0a
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-06-03 19:11:16 -0700

    proglem solving population issue

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var characterlength = 8;
 var choicearray = [];
 
 
-var lowerCaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var specialcharacters = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCaselettersarray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaselettersarray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialcharactersarray = ['@', '%', '+', '!', '#', '$', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
+var numbersarray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
 
@@ -17,12 +17,14 @@ var generateBtn = document.querySelector("#generate");
 
 
 
+
+
 //password randomization
 function generatePassword(){
-  var password = "";
+  var randompassword = "";
   for (var i = 0; i < characterlength; i++){
-    var random = (Math.random() * choicearray.length)
-    password = password + choicearray[random];
+    var random = math.floor(Math.random() * choicearray.length)
+    randompassword = password + choicearray[random];
 }
 return password;
 
@@ -30,37 +32,37 @@ return password;
 //Prompts
 function getprompts(){
 
-  characterlength = parseintprompt("Choose how many characters that you would like to include up to 128");
-  
+  characterlength = parseint(prompt("Choose how many characters that you would like to include up to 128"));
+
   if(isNaN(characterlength) || characterlength > 128 ||   characterlength < 8){
-  return false;
+    return false;
+
+  }
+ 
 }
 
+
   if (confirm("Would you like to include lower case letters?")) {
-    choicearr = choicearray.concat(lowerCaseletters);
+    choicearray = choicearray.concat(lowerCaselettersarray);
   }
   if (confirm("Would you like to include upper case letters?")) {
-    choicearr = choicearray.concat(upperCaseletters);
+    choicearray = choicearray.concat(upperCaselettersarray);
   }
   if (confirm("Would you like to include special letters?")) {
-    choicearray = choicearray.concat(specialcharacters);
+    choicearray = choicearray.concat(specialcharactersarray);
   }
   if (confirm("Would you like to include numbers?")) {
-    choicearray = choicearray.concat(numbers);
-  }
-
-    return true;
+    choicearray = choicearray.concat(numbersarray);
   }
 
 
      
 // Write password to the #password input
-function writePassword() 
-    
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
